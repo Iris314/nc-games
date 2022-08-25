f22ee06ca4ad6d7e1d78052a228e6fc77220526b
iris <25711391+Iris314@users.noreply.github.com>
2022-08-25 20:26:30 +0100

    add context toggle layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Header = () => {
   const { CurrentUser } = useContext(UserContext);
+  const { CurrentTheme, setCurrentTheme } = useContext(ThemeContext);
+
+  const toggleMode = () => {
+    setCurrentTheme(!CurrentTheme);
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <span className="header">
-        <Link className="userBanner" to="./users">
-          <img src={CurrentUser.avatar_url} alt="avatar"></img>
-          <span className="tooltiptext">{CurrentUser.username}</span>
-        </Link>
+        <span className="topHeader">
+          <label className="themeSwitch">
+            <input type="checkbox" onClick={toggleMode} />
+            <span className="slider"></span>
+          </label>
+          <Link className="userBanner" to="./users">
+            <img src={CurrentUser.avatar_url} alt="avatar"></img>
+            <span className="tooltiptext">{CurrentUser.username}</span>
+          </Link>
+        </span>
+
         <Link to="./">
           <h1>NC - Games</h1>
         </Link>
