17d489a735e907a12b5d4c8d98f1dc4711437aaf
iris <25711391+Iris314@users.noreply.github.com>
2022-08-26 14:50:09 +0100

    add styling to darkmode;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
@@ -11,30 +11,32 @@ const Header = () => {
     setCurrentTheme(!CurrentTheme);
   };
 
-  useEffect(() => {}, []);
-
   return (
     <>
-      <span className="header">
+      <header className={CurrentTheme ? "header dark" : "header"}>
         <span className="topHeader">
           <label className="themeSwitch">
             <input type="checkbox" onClick={toggleMode} />
             <span className="slider"></span>
           </label>
           <Link className="userBanner" to="./users">
-            <img src={CurrentUser.avatar_url} alt="avatar"></img>
-            <span className="tooltiptext">{CurrentUser.username}</span>
+            <div className="userBannerContainer">
+              <img src={CurrentUser.avatar_url} alt="avatar"></img>
+              <span className="tooltiptext">{CurrentUser.username}</span>
+            </div>
           </Link>
         </span>
+        <div className="title">
+          <Link to="./">
+            <h1>House of Games</h1>
+          </Link>
+        </div>
 
-        <Link to="./">
-          <h1>NC - Games</h1>
-        </Link>
         <span className="headerLinks">
           <Link to="/reviews">All Reviews</Link>
           <Link to="./reviews/new">New Review</Link>
         </span>
-      </span>
+      </header>
     </>
   );
 };
--- src/components/frontpage/RecentReview.js
+++ src/components/frontpage/RecentReview.js
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 
 const RecentReview = ({ review }) => {
   return (
-    <Link to={`/reviews/${review.review_id}`} className="container">
+    <Link
+      to={`/reviews/${review.review_id}`}
+      style={{
+        backgroundColor: "white",
+        opacity: "0.9",
+      }}>
       <h5>Most Recent Review</h5>
       <p>{review.title}</p>
       <p>By {review.owner}</p>
-      <span className="topDetails">
+      <span className="ratings">
+        <p>Comments: {review.comment_count}</p>
         <p>Votes: {review.votes}</p>
       </span>
     </Link>
